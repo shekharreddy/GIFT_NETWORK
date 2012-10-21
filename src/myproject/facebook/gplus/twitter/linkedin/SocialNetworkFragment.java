fe34af26d19cd744327a0bfcb44c828da2e6e502
SHEKHARREDDY <reddy.anddev@gmail.com>
2012-10-20 21:48:49 -0500

    removed unwanted import

--- src/myproject/facebook/gplus/twitter/linkedin/SocialNetworkFragment.java
+++ src/myproject/facebook/gplus/twitter/linkedin/SocialNetworkFragment.java
@@ -11,7 +11,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.HttpAuthHandler;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;

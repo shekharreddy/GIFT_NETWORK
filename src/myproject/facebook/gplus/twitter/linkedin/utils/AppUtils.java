89ca18a98d617177b5ac1d9e9dc97ca0b8844cd2
shekharreddy <reddy.anddev@gmail.com>
2014-05-10 10:22:57 -0500

    Update AppUtils.java
    Updated Google Plus URL

--- src/myproject/facebook/gplus/twitter/linkedin/utils/AppUtils.java
+++ src/myproject/facebook/gplus/twitter/linkedin/utils/AppUtils.java
@@ -9,7 +9,7 @@ public class AppUtils {
 	
 	//Social Network URLs
 	public static final String FACEBOOK_URL           = "http://www.facebook.com";
-	public static final String GOOGLEPLUS_URL         = "http://plus.google.com";
+	public static final String GOOGLEPLUS_URL         = "https://accounts.google.com/ServiceLogin?service=oz&passive=1209600&continue=https://plus.google.com/app/basic/stream?source%3Dapppromo%26gpsrc%3Dgpmobile:android&btmpl=mobile_tier2&ltmpl=tier2gplus";
 	public static final String LINKEDIN_URL           = "https://touch.www.linkedin.com/login.html";
 	public static final String TWITTER_URL            = "https://mobile.twitter.com/session/new";
 	
